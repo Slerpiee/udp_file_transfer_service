93e5a019bc981d75ddddf734f26204b93dfb4fb9
Slerpiee <leonid.d.polyakov@gmail.com>
2025-09-27 21:40:49 +0300

    init commit

---
+++ client.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+type packet_file struct{
+    byte_file []byte
+    packet_count []byte
+	md5 []byte
+	name []byte
+	splitted_file [][]byte
+	unique []byte
+}
+
+
+var global_file packet_file
+
+var client net.PacketConn
+var remoteAddr *net.UDPAddr
+
+
+func init_file(){
+	path := os.Args[1]
+	file, err := os.Open(path)
+    if err != nil{
+        log.Fatal(err)
+    }
+    stat, _ := file.Stat()
+    global_file.byte_file = make([]byte, stat.Size())
+    file.Read(global_file.byte_file)
+	global_file.name = []byte(file.Name())
+	massive := []byte{}
+	for ind, el := range global_file.byte_file{
+		if ind % 6000 == 0 && ind != 0{
+			global_file.splitted_file = append(global_file.splitted_file, massive)
+			massive = []byte{}
+		}
+		massive = append(massive, el)
+	}
+	if len(massive) != 0{
+		global_file.splitted_file = append(global_file.splitted_file, massive)
+	}
+	binary.BigEndian.PutUint64(global_file.packet_count, uint64(len(global_file.splitted_file)))
+	global_file.md5 = get_md5(global_file.byte_file)
+}
+
+
+func get_md5(data[]byte) (total []byte){
+	for _, el := range md5.Sum(data){
+		total = append(total, el)
+	}
+	fmt.Println(hex.EncodeToString(total))
+	return 
+}
+
+
+func wait_for_answer(err chan struct{}, answer chan []byte) {
+	time.AfterFunc(3*time.Second, func(){
+		err <- struct{}{}
+	})
+	buffer := make([]byte, 65535)
+	byt, adr, er := client.ReadFrom(buffer)
+	if er != nil{
+		log.Fatal(er)
+	}
+	log.Printf("Accept: %v bytes from %v", byt, adr)
+	data := make([]byte, byt)
+	copy(data, buffer[:byt])
+	answer <- data
+
+}
+
+
+func send_start_package(){
+	startpackage := []byte{0}
+	startpackage = append(startpackage, global_file.packet_count...)
+	startpackage = append(startpackage, global_file.md5...)
+	startpackage = append(startpackage, global_file.name...)
+	err_chan := make(chan struct{})
+	data_chan := make(chan []byte)
+	client.WriteTo(startpackage, remoteAddr)
+	go wait_for_answer(err_chan, data_chan)
+	for{
+		select{
+		case <- err_chan:
+			log.Fatal("Time Expired, Server is not Responding")
+			return
+		case data := <- data_chan:
+			fmt.Println("OK - uuid")
+			global_file.unique = append(global_file.unique, data...)
+			return
+		}
+	}
+	
+}
+
+func send_packets(){
+	fmt.Println("Packets for file:", len(global_file.splitted_file))
+	for ind, el := range global_file.splitted_file{
+		i := ind
+		pack := []byte{1}
+		pack = append(pack, global_file.unique...)
+		num := make([]byte, 16)
+		binary.BigEndian.PutUint64(num, uint64(i))
+		pack = append(pack, num...)
+		pack = append(pack, el...)
+		client.WriteTo(pack, remoteAddr)
+		time.Sleep(1*time.Millisecond)
+	}
+}
+
+
+
+func init(){
+	var port string 
+	fmt.Scanf("Введите порт сервера: %s", &port)
+	addr := "127.0.0.1:" + port
+	remoteAddr, _ = net.ResolveUDPAddr("udp", addr)
+	global_file.md5 = make([]byte, 16)
+	global_file.packet_count = make([]byte, 16)
+}
+
+
+
+func main() {
+	if len(os.Args) != 2{
+		log.Fatal("Введите имя файла")
+	}
+	con, err := net.ListenPacket("udp", "")
+	client = con
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	init_file()
+	send_start_package()
+	send_packets()
+}
---
+++ server.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+	"os"
+	"crypto/md5"
+	"github.com/google/uuid"
+	"encoding/hex"
+)
+
+
+var server net.PacketConn
+var mutex sync.Mutex
+
+
+type user struct{
+	file_name string
+	packet_count int
+	packets map[int][]byte
+	md5 []byte
+}
+var users map[string]user
+
+
+var time_channels map[string] chan int
+
+
+func handle_request(data []byte, addr net.Addr){
+	switch data[0]{
+	case 0:
+		fmt.Println("Start package arrived from: " + addr.String())
+		uuid := uuid.New()
+		send := make([]byte, 36)
+		copy(send, []byte(uuid.String()))
+		server.WriteTo(send, addr)
+		newuser := user{}
+		newuser.md5 = data[17:33]
+		newuser.packet_count = int(binary.BigEndian.Uint64(data[1:16]))
+		newuser.file_name = string(data[33:])
+		newuser.packets = map[int][]byte{}
+		fmt.Println(newuser)
+		mutex.Lock() 
+		users[uuid.String()] = newuser
+		time_channels[uuid.String()] = make(chan int)
+		mutex.Unlock()
+		go timer(uuid.String())
+	case 1:
+		uid := string(data[1:37])
+
+		
+		mutex.Lock()
+		_, ok := users[uid]
+		if !ok{
+			fmt.Println("No such user auth")
+			return
+		}
+		mutex.Unlock()
+
+
+		num := int(binary.BigEndian.Uint64(data[37:53]))
+		pack := data[53:]
+
+		fmt.Println("Package:", num+1, "for: ", uid)
+
+		mutex.Lock()
+		channel := time_channels[uid]
+		mutex.Unlock()
+
+		channel <- 1
+
+		mutex.Lock()
+		users[uid].packets[num] = pack
+		user := users[uid]
+		mutex.Unlock()
+		if len(user.packets) == user.packet_count{
+			mutex.Lock()
+			c := time_channels[uid]
+			mutex.Unlock()
+			c <- 0
+			mutex.Lock()
+			delete(time_channels, uid)
+			mutex.Unlock()
+			file := []byte{}
+			for i:=0; i<user.packet_count;i++{
+				num = i
+				file=append(file, user.packets[num]...)
+			}
+			if hex.EncodeToString(user.md5) == hex.EncodeToString(get_md5(file)){
+				fmt.Println("BUILD FILE FOR: "+uid)
+				build_file(file, user.file_name, uid)
+			} else{
+				fmt.Println("hash sum compare failed")
+			}
+			mutex.Lock()
+			delete(users, uid)
+			mutex.Unlock()
+		}
+	}
+	
+}
+
+
+func get_md5(data[]byte) (total []byte){
+	for _, el := range md5.Sum(data){
+		total = append(total, el)
+	}
+	return 
+}
+
+
+func build_file(file []byte, name string, hex string) {
+	_, err := os.Stat(name)
+	if err == nil {
+		name = hex + name
+	}
+	f, err := os.Create(name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	_, e := f.Write(file)
+	if e != nil {
+		fmt.Println(e)
+		return
+	}
+
+}
+
+
+func timer(id string){
+	for{
+		mutex.Lock()
+		channel := time_channels[id]
+		mutex.Unlock()
+
+		select{
+		case x := <- channel:
+			if x == 1{
+				go timer(id)
+				fmt.Println("timer resetted for: "+id)
+			}
+			return
+		case <- time.After(5*time.Second):
+			fmt.Println("Timer ended for: "+id)
+			mutex.Lock()
+			delete(users, id)
+			mutex.Unlock()
+			return
+		}
+	}
+}
+
+
+func init(){
+	users = map[string]user{}
+	time_channels = map[string]chan int{}
+	err := os.Mkdir("storage", 0750)
+	if err != nil && !os.IsExist(err) {
+		fmt.Println("Already exists")
+	}
+	os.Chdir("storage")
+}
+
+
+func main(){
+	var port string 
+	fmt.Scanf("Введите порт сервера, который будет слушать пакеты: %s", &port)
+	addr := "127.0.0.1:" + port
+	con, err := net.ListenPacket("udp", addr)
+	server = con
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server started on:", server.LocalAddr().String())
+	defer server.Close()
+
+	buffer := make([]byte, 65535)
+
+	for {
+		bytesRead, remoteAddr, err := server.ReadFrom(buffer)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Read: %v bytes from %v\n", bytesRead, remoteAddr)
+		data := make([]byte, bytesRead)
+		copy(data, buffer[:bytesRead])
+		go handle_request(data, remoteAddr)
+	}
+}
